cd4c4694d53e4b2ff5e891b00227bb12c688191d
arrogance231 <alchemyofjhine@gmail.com>
2025-02-21 14:32:50 +0800

    feat: fix

--- frontend/app/browse/page.tsx
+++ frontend/app/browse/page.tsx
@@ -1,36 +1,37 @@
-// /app/dashboard/page.tsx
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import useAuth from "@/hooks/useAuth";
-
-export default function Dashboard() {
-  const { user, loading, logout } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!loading && !user) {
-      router.replace("/login");
-    }
-  }, [user, loading, router]);
-
-  if (loading) return <p>Loading...</p>;
-  if (!user) return null; // while redirecting
-
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { navLinks } from "@/constants/index";
+export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white space-y-4">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-      <p>Welcome, {user.email}</p>
-      <button
-        onClick={async () => {
-          await logout();
-          router.refresh();
-          router.replace("/login");
-        }}
-        className="p-2 bg-red-600 rounded hover:bg-red-700 font-semibold"
-      >
-        Logout
-      </button>
-    </div>
+    <main className="relative h-screen">
+      <nav className="flex justify-between bg-[#4A6397] h-[60px]">
+        <div className="my-auto flex gap-4">
+          <Image src="./icon.svg" alt="icon" width={50} height={50}></Image>
+          <Input
+            placeholder="Search Jobs"
+            className="text-white bg-[#6D84B3] text-center"
+          ></Input>
+        </div>
+        <div className="justify-center h-full items-center">
+          <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden font-bold relative h-full">
+            {navLinks.map((item) => (
+              <li key={item.label} className="flex items-center gap-2">
+                <Image src={item.img} alt={item.label} width={20} height={20} />
+                <a
+                  href={item.href}
+                  className="font-bold size-8xl font-montserrat leading-normal text-lg bg-gradient-to-r from-orange-400 to-teal-200 bg-clip-text text-transparent animate-text"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="px-12 h-full flex justify-center items-center max-lg:hidden">
+          <Button>Join Us</Button>
+        </div>
+      </nav>
+    </main>
   );
 }
